a5d4831e898cbf8221a3767cbf1ae87a2cb89b2f
Jingwen Peng <pengsrc@outlook.com>
2018-04-20 15:25:16 +0800

    Remove all styles
    Signed-off-by: Jingwen Peng <pengsrc@outlook.com>

--- src/layouts/index.jsx
+++ src/layouts/index.jsx
@@ -3,7 +3,6 @@ import PropTypes from 'prop-types';
 import Helmet from 'react-helmet';
 
 import Header from '../components/header';
-import './index.scss';
 
 const Layout = ({ children, data }) => (
   <div>
@@ -16,14 +15,7 @@ const Layout = ({ children, data }) => (
       ]}
     />
     <Header siteTitle={data.site.siteMetadata.title} />
-    <div
-      style={{
-        margin: '0 auto',
-        maxWidth: 960,
-        padding: '0px 1.0875rem 1.45rem',
-        paddingTop: 0,
-      }}
-    >
+    <div>
       {children()}
     </div>
   </div>
--- src/pages/page-2.jsx
+++ src/pages/page-2.jsx
@@ -7,6 +7,6 @@ const SecondPage = () => (
     <p>Welcome to page 2</p>
     <Link to="/">Go back to the homepage</Link>
   </div>
-)
+);
 
 export default SecondPage;
