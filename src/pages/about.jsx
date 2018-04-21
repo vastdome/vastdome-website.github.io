5ef2d5dc886ed9c400ac99bdc96680df04e12030
Jingwen Peng <pengsrc@outlook.com>
2018-04-21 18:44:56 +0800

    Add about page
    Signed-off-by: Jingwen Peng <pengsrc@outlook.com>

---
+++ src/pages/about.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './about.scss';
+
+const About = () => (
+  <div className="container section">
+    <div id="about">
+      <h1>About</h1>
+    </div>
+  </div>
+);
+
+export default About;
