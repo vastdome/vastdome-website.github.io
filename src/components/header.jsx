848e927b5ccc51ab5bc520e72b9a639f9c359f58
Jingwen Peng <pengsrc@outlook.com>
2018-04-22 22:52:51 +0800

    Update header ready for mobile
    Signed-off-by: Jingwen Peng <pengsrc@outlook.com>

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,12 +1,17 @@
+import _ from 'lodash';
 import React from 'react';
 import PropTypes from 'prop-types';
 import Link from 'gatsby-link';
 import classnames from 'classnames';
 import {
   Alignment,
-  Button,
-  Navbar, NavbarGroup, NavbarHeading,
+  Button, ButtonGroup,
+  Navbar,
+  NavbarGroup,
+  NavbarHeading,
+  Icon,
 } from '@blueprintjs/core';
+import { stack as Menu } from 'react-burger-menu';
 
 import './header.scss';
 
@@ -17,15 +22,50 @@ class Header extends React.Component {
     this.state = {
       currentPath: '',
       currentFullPath: '',
+      isHome: true,
+      isOnTop: true,
+      isBurgerMenuOpen: false,
     };
+
+    this.scrollTimer = null;
+
+    this.handleScroll = this.handleScroll.bind(this);
+    this.handleBurgerMenuStateChange = this.handleBurgerMenuStateChange.bind(this);
+    this.closeBurgerMenu = this.closeBurgerMenu.bind(this);
   }
 
   componentDidMount() {
     this.updateCurrentPath();
+    this.checkIsHome();
+    this.checkIsOnTop();
+
+    window.addEventListener('scroll', this.handleScroll);
   }
 
   componentDidUpdate() {
     this.updateCurrentPath();
+    this.checkIsHome();
+    this.checkIsOnTop();
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('scroll', this.handleScroll);
+  }
+
+  handleScroll() {
+    if (this.scrollTimer === null) {
+      this.checkIsOnTop();
+    } else {
+      setTimeout(() => { this.scrollTimer = null; }, 100);
+    }
+  }
+
+  handleBurgerMenuStateChange(state) {
+    this.setState({ isBurgerMenuOpen: state.isOpen });
+  }
+
+  closeBurgerMenu() {
+    this.setState({ isBurgerMenuOpen: false });
   }
 
   updateCurrentPath() {
@@ -40,46 +80,135 @@ class Header extends React.Component {
     }
   }
 
+  checkIsHome() {
+    this.updateCurrentPath();
+
+    const isHome = this.state.currentPath === '/';
+    if (this.state.isHome !== isHome) {
+      this.setState({ isHome });
+    }
+  }
+
+  checkIsOnTop() {
+    const supportPageOffset = window.pageXOffset !== undefined;
+    const isCSS1Compat = ((document.compatMode || '') === 'CSS1Compat');
+
+    let offset = 0;
+    if (supportPageOffset) {
+      offset = window.pageYOffset;
+    } else {
+      offset = isCSS1Compat ? document.documentElement.scrollTop : document.body.scrollTop;
+    }
+
+    const isOnTop = offset <= 0;
+    if (this.state.isOnTop !== isOnTop) {
+      this.setState({ isOnTop });
+    }
+
+    // Additional steps for burger menu.
+    const burgerButtons = document.getElementsByClassName('bm-burger-button');
+    _.map(burgerButtons, (button) => {
+      if (isOnTop) {
+        button.classList.remove('smaller');
+      } else {
+        button.classList.add('smaller');
+      }
+    });
+  }
+
   render() {
+    const links = (
+      <ButtonGroup className="pt-dark">
+        <Link to="/#features">
+          <Button
+            className="pt-minimal pt-ui-text"
+            icon="properties"
+            text="Features"
+            active={this.state.currentFullPath.startsWith('/#features')}
+            onClick={this.closeBurgerMenu}
+          />
+        </Link>
+        <Link to="/#about">
+          <Button
+            className="pt-minimal pt-ui-text"
+            icon="info-sign"
+            text="About"
+            active={this.state.currentFullPath.startsWith('/about/')}
+            onClick={this.closeBurgerMenu}
+          />
+        </Link>
+        <Link to="/#download">
+          <Button
+            className="pt-minimal pt-ui-text"
+            icon="download"
+            text="Download"
+            onClick={this.closeBurgerMenu}
+          />
+        </Link>
+        <Link to="/#contact">
+          <Button
+            className="pt-minimal pt-ui-text"
+            icon="chat"
+            text="Contact"
+            onClick={this.closeBurgerMenu}
+          />
+        </Link>
+      </ButtonGroup>
+    );
+
     return (
-      <Navbar
-        id="navbar"
-        className={(() => {
-          const cls = ['pt-dark'];
-          if (this.state.currentPath === '/') {
-            cls.push('transparent');
-          }
-          return classnames(cls);
-        })()}
-      >
-        <div id="navbar-items" className="container">
-          <NavbarGroup id="navbar-brand" align={Alignment.LEFT}>
-            <NavbarHeading>
-              <Link to="/">{this.props.siteTitle}</Link>
-            </NavbarHeading>
-          </NavbarGroup>
-          <NavbarGroup align={Alignment.RIGHT}>
-            <Link to="/#features">
-              <Button
-                className="pt-minimal pt-ui-text"
-                icon="properties"
-                text="Features"
-                active={this.state.currentFullPath.startsWith('/#features')}
+      <div id="navbar-floor">
+        <Navbar
+          id="navbar"
+          className={(() => {
+            const cls = ['pt-dark'];
+            if (this.state.isOnTop) {
+              if (this.state.isHome) {
+                cls.push('transparent');
+              }
+            } else {
+              cls.push('smaller');
+            }
+            return classnames(cls);
+          })()}
+        >
+          <div className="container">
+            <NavbarGroup id="navbar-brand" align={Alignment.LEFT}>
+              <NavbarHeading>
+                <Link to="/">{this.props.siteTitle}</Link>
+              </NavbarHeading>
+            </NavbarGroup>
+            <NavbarGroup id="navbar-items" align={Alignment.RIGHT}>
+              <ButtonGroup className="pt-dark">
+                {links}
+              </ButtonGroup>
+            </NavbarGroup>
+          </div>
+        </Navbar>
+        <div id="navbar-burger">
+          <Menu
+            isOpen={this.state.isBurgerMenuOpen}
+            onStateChange={this.handleBurgerMenuStateChange}
+            right
+            width="240px"
+            customBurgerIcon={
+              <Icon
+                className={(() => {
+                  const cls = [];
+                  if (!this.state.isOnTop) {
+                    cls.push('smaller');
+                  }
+                  return classnames(cls);
+                })()}
+                icon="menu"
+                iconSize={30}
               />
-            </Link>
-            <Button className="pt-minimal pt-ui-text" icon="document" text="Docs" />
-            <Button className="pt-minimal pt-ui-text" icon="download" text="Download" />
-            <Link to="/about/">
-              <Button
-                className="pt-minimal pt-ui-text"
-                icon="info-sign"
-                text="About"
-                active={this.state.currentFullPath.startsWith('/about/')}
-              />
-            </Link>
-          </NavbarGroup>
+            }
+          >
+            <ButtonGroup className="pt-dark">{links}</ButtonGroup>
+          </Menu>
         </div>
-      </Navbar>
+      </div>
     );
   }
 }
