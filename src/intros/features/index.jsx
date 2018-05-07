ab0d0492b2ec94892e110e9496a2607157ef6753
Jingwen Peng <pengsrc@outlook.com>
2018-05-07 22:32:30 +0800

    Improve features styles
    Signed-off-by: Jingwen Peng <pengsrc@outlook.com>

--- src/intros/features/index.jsx
+++ src/intros/features/index.jsx
@@ -12,31 +12,43 @@ const Features = () => {
   const features = [{
     title: 'Make Blockchain Simple',
     image: imageSimple,
+    backgroundColor: '#fff9db',
+    color: '#7d7968',
   }, {
     title: 'Application Oriented Design',
     image: imageApplication,
+    backgroundColor: '#8ccffa',
+    color: '#48677d',
   }, {
     title: 'Embrace Existing Ecology',
     image: imageGear,
+    backgroundColor: '#ecd5d4',
+    color: '#7d7070',
   }, {
     title: 'Autonomous Cloud with Token Economy',
     image: imageCloud,
+    backgroundColor: '#deffb9',
+    color: '#677d52',
   }];
 
   return (
     <div className="features container section">
-      <h1>Features</h1>
+      <h1>Awesome Features</h1>
       <div className="space size-2" />
       {
         _.map(features, feature => (
           <div key={feature.title} className="feature">
-            <div
-              className="illustrate"
-              style={{
-                backgroundImage: `url("${feature.image}")`,
-              }}
-            />
-            <h3>{feature.title}</h3>
+            <div className="card">
+              <div style={{ background: feature.backgroundColor }}>
+                <div
+                  className="illustrate"
+                  style={{
+                    backgroundImage: `url("${feature.image}")`,
+                  }}
+                />
+              </div>
+              <h3 style={{ color: feature.color }}>{feature.title}</h3>
+            </div>
           </div>
         ))
       }
