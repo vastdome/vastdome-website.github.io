097dfb794740a4aeb3a8451f6d54f1500dd2c832
Jingwen Peng <pengsrc@outlook.com>
2018-05-12 19:23:34 +0800

    Rearrange details section
    Signed-off-by: Jingwen Peng <pengsrc@outlook.com>

--- src/intros/details/index.jsx
+++ src/intros/details/index.jsx
@@ -14,50 +14,59 @@ import imageShoppingCart from './shopping-cart.svg';
 
 const Details = () => {
   const details = [{
-    text: 'Standalone Blockchain for Each Application',
     image: imageLayers,
+    title: 'Standalone Blockchain for Each Application',
+    text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco..',
   }, {
-    text: 'Application Blockchain Development Kit',
     image: imageWand,
+    title: 'Application Blockchain DevKit',
+    text: 'Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae.',
   }, {
-    text: 'Scalable Blockchain Node',
     image: imageNetwork,
+    title: 'Scalable Blockchain Node',
+    text: 'Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt.',
   }, {
-    text: 'Off-chain Services',
     image: imageShapes,
+    title: 'Off-chain Services',
+    text: 'Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur.',
   }, {
-    text: 'Blockchain Services',
     image: imageLegoBlock,
+    title: 'Blockchain Services',
+    text: 'Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla.',
   }, {
-    text: 'Compatible with Existing Public Blockchains',
     image: imagePlug,
+    title: 'Compatible with Existing Public Blockchains',
+    text: 'Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
   }, {
-    text: 'Delegate Market',
     image: imageBuilding,
+    title: 'Delegate Market',
+    text: 'Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci.',
   }, {
-    text: 'Application Market',
     image: imageShoppingCart,
+    title: 'Application Market',
+    text: 'Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla.',
   }];
 
   return (
     <div className="details container section">
       <h1>Here&apos;s how Vastdome works</h1>
       <div className="space size-2" />
-      {
-        _.map(details, detail => (
-          <div key={detail.text} className="detail">
-            <div
-              className="illustrate"
-              style={{
-                backgroundImage: `url("${detail.image}")`,
-              }}
-            />
-            <div className="text">
-              <p>{detail.text}</p>
-            </div>
-          </div>
-        ))
-      }
+      <ul>
+        {
+          _.map(details, detail => (
+            <li key={detail.title} className="detail">
+              <div
+                className="illustrate"
+                style={{
+                  backgroundImage: `url("${detail.image}")`,
+                }}
+              />
+              <p className="title">{detail.title}</p>
+              <p className="text">{detail.text}</p>
+            </li>
+          ))
+        }
+      </ul>
     </div>
   );
 };
