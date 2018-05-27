116d170e6982af368719f66f776124666e98da66
Jingwen Peng <pengsrc@outlook.com>
2018-05-27 09:02:01 +0800

    Add architecture image
    Signed-off-by: Jingwen Peng <pengsrc@outlook.com>

--- src/intros/architecture/index.jsx
+++ src/intros/architecture/index.jsx
@@ -1,8 +1,15 @@
 import React from 'react';
 
+import './index.scss';
+
+import imageArchitecture from './architecture.png';
+
 const Architecture = () => (
-  <div className="contacts container section">
+  <div className="architecture container section">
     <h1>Architecture</h1>
+    <div className="content">
+      <img className="image pt-card pt-elevation-3" alt="Architecture" src={imageArchitecture} />
+    </div>
   </div>
 );
 
